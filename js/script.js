4ce78416aa974bd35275c6481ed82957ff556f47
Gaurav Singh <gauravnegi9634@gmail.com>
2021-10-21 17:15:52 +0530

    Fixed scroll to top bug

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-//Scroll to top
-var mybutton = document.getElementById("ScrollUpBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 //Dark Mode
 function DarkMode() {
   var element = document.body;
